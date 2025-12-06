29666a476231cd0b24393c8c3bead6b27c8ad927
hom-cv <chaithatvanasrisawasd@gmail.com>
2025-12-06 17:19:21 +0700

    xss handling

--- backend/app/services/email_service.py
+++ backend/app/services/email_service.py
@@ -1,5 +1,6 @@
 """Email service for sending emails via SendGrid."""
 
+from backend.app.templates.email_verification import get_verification_email_html
 import logging
 
 from python_http_client.exceptions import HTTPError
@@ -8,7 +9,6 @@ from sendgrid.helpers.mail import Mail
 
 from app.core.jwt import create_email_verification_token
 from app.core.settings import Settings, get_settings
-from app.templates.email_verification import get_verification_email_html
 
 logger = logging.getLogger(__name__)
 settings: Settings = get_settings()
