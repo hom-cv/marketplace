fdaad81165fa853e7f1a1987f40d16bae2637f0f
hom-cv <chaithatvanasrisawasd@gmail.com>
2026-05-28 02:17:58 +0700

    Comment out idempotency for create user

--- backend/app/services/seller_service.py
+++ backend/app/services/seller_service.py
@@ -80,7 +80,7 @@ class SellerService:
         try:
             account = await self.stripe_service.create_connect_account(
                 email=user.email_address,
-                idempotency_key=f"acct-v7-{user.id}",
+                # idempotency_key=f"acct-{user.id}",
             )
             link = await self.stripe_service.create_account_link(
                 account_id=account.id,
